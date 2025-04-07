ad1e93f8071a7367dcac39159de5f472395543f8
soohyeon <meltapplee@gmail.com>
2025-04-07 14:39:09 +0900

    add :: eslint 추가

---
+++ eslint.config.mjs
@@ -0,0 +1,42 @@
+import tseslint from 'typescript-eslint';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+
+export default [
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  },
+
+  {
+    files: ['**/*.{ts,tsx,js,jsx}'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: new URL('.', import.meta.url),
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+
+      'prettier/prettier': ['error', {
+        singleQuote: true,
+        semi: true,
+        trailingComma: 'all',
+        printWidth: 100,
+        tabWidth: 2,
+      }],
+    },
+  },
+
+  {
+    rules: prettierConfig.rules,
+  },
+];
