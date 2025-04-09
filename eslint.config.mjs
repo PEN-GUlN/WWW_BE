6bfc7a2ca0ff0fcd7339f771cf080f8d21c450fb
soohyeon <meltapplee@gmail.com>
2025-04-09 11:48:05 +0900

    setting :: prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,38 +1,41 @@
-import tseslint from 'typescript-eslint';
-import prettierPlugin from 'eslint-plugin-prettier';
-import prettierConfig from 'eslint-config-prettier';
+import tseslint from "typescript-eslint";
+import prettierPlugin from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
 
 export default [
   {
-    ignores: ['dist/**', 'node_modules/**'],
+    ignores: ["dist/**", "node_modules/**"],
   },
 
   {
-    files: ['**/*.{ts,tsx,js,jsx}'],
+    files: ["**/*.{ts,tsx,js,jsx}"],
     languageOptions: {
       parser: tseslint.parser,
       parserOptions: {
-        project: './tsconfig.json',
-        tsconfigRootDir: new URL('.', import.meta.url),
-        sourceType: 'module',
+        project: "./tsconfig.json",
+        tsconfigRootDir: new URL(".", import.meta.url),
+        sourceType: "module",
       },
     },
     plugins: {
-      '@typescript-eslint': tseslint.plugin,
+      "@typescript-eslint": tseslint.plugin,
       prettier: prettierPlugin,
     },
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/explicit-function-return-type': 'off',
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
 
-      'prettier/prettier': ['error', {
-        singleQuote: true,
-        semi: true,
-        trailingComma: 'all',
-        printWidth: 100,
-        tabWidth: 2,
-      }],
+      "prettier/prettier": [
+        "error",
+        {
+          singleQuote: true,
+          semi: true,
+          trailingComma: "all",
+          printWidth: 100,
+          tabWidth: 2,
+        },
+      ],
     },
   },
 
