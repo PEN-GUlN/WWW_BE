cc76bd54b03557862cb6f20e70e3309f1336579d
soohyeon <meltapplee@gmail.com>
2025-04-09 11:48:18 +0900

    setting :: DB 연결 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: parseInt(process.env.DB_PORT),
+    }),
+  ],
 })
 export class AppModule {}
