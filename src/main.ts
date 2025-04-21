c74a21d0e2f18de81733559e47dbfb1c3e364c38
soohyeon <meltapplee@gmail.com>
2025-04-21 15:13:04 +0900

    chore:: 세션 관련 코드 추가

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import * as session from 'express-session';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.use(
+    session({
+      secret: 'asdfasdkfalsdjlfkajsd',
+      resave: false,
+      saveUninitialized: false,
+      cookie: { maxAge: 1000 * 60 * 60 },
+    }),
+  );
+
+  await app.listen(8880);
 }
 bootstrap();
