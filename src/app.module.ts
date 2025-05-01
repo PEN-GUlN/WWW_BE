0486dde0155945f04815b49872a2ddfa73221637
soohyeon <meltapplee@gmail.com>
2025-05-01 10:08:46 +0900

    lint

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,6 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
 import { CommentModule } from './comment/comment.module';
-import { Bookmark } from './bookmark/entity/bookmark.entity';
 import { BookmarkModule } from './bookmark/bookmark.module';
 
 @Module({
