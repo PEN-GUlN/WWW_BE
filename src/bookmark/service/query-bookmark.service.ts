89a4d9c57a276308c958a66bc11fea075de1f5ea
soohyeon <meltapplee@gmail.com>
2025-07-04 09:42:53 +0900

    chore :: 북마크 조회시 countryCode반환

--- src/bookmark/service/query-bookmark.service.ts
+++ src/bookmark/service/query-bookmark.service.ts
@@ -57,6 +57,7 @@ export class QueryBookmarkService {
       isAgency: job.isAgency,
       employmentType: job.employmentType,
       location: job.location,
+      countryCode: job.countryCode,
       publishedDate: this.getDaysSincePublished(job.publishedDate),
       experienceLevel: job.experienceLevel,
       isBookmarked,
